fcb87b4c96c3c4086e46f7155352a927a0ce370f
Alex <alexandersuglio@gmail.com>
2017-05-04 17:33:44 -0400

    completed item inquiry prompt still working on how to update item inventory on sql db from node app

--- Bamazon.js
+++ Bamazon.js
@@ -8,15 +8,18 @@ var connection = mysql.createConnection({
     database: "bamazon_db",
 });
 
+//makes connection with db
 connection.connect(function(err) {
     if (err) {
         throw err;
         console.log("ERROR: " + err);
     }
-    // console.log("connection successful")
+ // console.log("connection successful")
 
 });
 
+//prompt to make item selection
+//still wanna create else statement if a product is entered that's not in the database
 inquirer.prompt([
 
     {
@@ -26,19 +29,20 @@ inquirer.prompt([
 
 ]).then(function(answers) {
 
-    // var i; 
+    // selects particular item from db
     connection.query("select * from surf_gear where product = ?", answers.name, function(error, response) {
         if (error) {
             throw error
         } else {
 
+ //for loop which helps pick out specific item
             for (var i = 0; i < response.length; i++) {
-
-                console.log("Product ID: " + response[i].Product_ID + "\nProduct: " + response[i].Product + "\nPrice: $" + response[i].Price + "\nDepartment: " + response[i].Department + "\nNumber Left In Stock: " + response[i].Stock);
+//results below
+               console.log("Product ID: " + response[i].Product_ID + "\nProduct: " + response[i].Product + "\nPrice: $" + response[i].Price + "\nDepartment: " + response[i].Department + "\nNumber Left In Stock: " + response[i].Stock);
             }
         }
 
-
+ //second prompt to ask user amount they wanna buy
         inquirer.prompt([
 
             {
@@ -51,6 +55,7 @@ inquirer.prompt([
             console.log("You wanna buy " + answers.name2);
             console.log(response[i].Stock);
 
+ //if else statement below I'm 'betaing' 
 
             // if (answers.name2 > response[i].Stock) {
             //     console.log("insuffient stock");
