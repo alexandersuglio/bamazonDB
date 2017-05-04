3ae9039f8becf2053b470a15b4bd102027e4ccfc
Alex <alexandersuglio@gmail.com>
2017-05-04 17:22:25 -0400

    Successfully conencted SQL DB to node file, working on inventory update function

--- Bamazon.js
+++ Bamazon.js
@@ -8,50 +8,64 @@ var connection = mysql.createConnection({
     database: "bamazon_db",
 });
 
-
-inquirer.prompt([
-    // var entry = process.argv[2];
-
-    {
-        name: "name",
-        message: "Enter Product ID"
+connection.connect(function(err) {
+    if (err) {
+        throw err;
+        console.log("ERROR: " + err);
     }
-    // {
-    //     name: "name2",
-    //     message: "How many do you wish to purchase?"
-    // }
-
-]).then(function(answers) {
-var x = event.keyCode;
-
-    if (answers.name < 1 || answers.name > 10 ) {
-        console.log("No item found");
-    } else {
-        console.log("This product exists");
-        connection.connect(function(err) {
-            if (err) {
-                throw err;
-            } else {
-                console.log("connected");
-                console.log("surf_gear");
-            }
-        });
-// var name = answers.name;        
-// connection.query("SELECT product FROM surf_gear", function(err, res) {
-//     if (err) throw err;
-//     // console.log(res);
-//     console.log(res);
-// });
-    }
+    // console.log("connection successful")
 
 });
 
+inquirer.prompt([
+
+    {
+        message: "Enter product",
+        name: "name"
+    }
+
+]).then(function(answers) {
+
+    // var i; 
+    connection.query("select * from surf_gear where product = ?", answers.name, function(error, response) {
+        if (error) {
+            throw error
+        } else {
+
+            for (var i = 0; i < response.length; i++) {
+
+                console.log("Product ID: " + response[i].Product_ID + "\nProduct: " + response[i].Product + "\nPrice: $" + response[i].Price + "\nDepartment: " + response[i].Department + "\nNumber Left In Stock: " + response[i].Stock);
+            }
+        }
 
 
+        inquirer.prompt([
 
-// connection.query("SELECT PRODUCT, PRICE FROM surf_gear", function(error, result){
-//     if (error) throw error;
-//     console.log(result);
-// })
+            {
+                message: "How many do you want to buy?",
+                name: "name2"
+            }
+
+        ]).then(function(answers) {
+
+            console.log("You wanna buy " + answers.name2);
+            console.log(response[i].Stock);
 
 
+            // if (answers.name2 > response[i].Stock) {
+            //     console.log("insuffient stock");
+
+            // } else {
+            //     console.log("Remaining Stock: " + response[i].Stock - answers.name2);
+            // }
+            // for (var j; j < response.length; j++) {
+            //     // console.log("you wanna buy " + answers.name2);
+            //     console.log(response[j].Stock);
+            // }
+        });
+        // if (answers.name === undefined) {
+        //     console.log("Item not found, please try again");
+        // }
+
+    })
+});
